bd877b05614e98a4ca7c3cf93cfe3a9b5ff5542f
Haseeb <chhaseebamjad0@gmail.com>
2026-05-17 17:27:55 +0500

    fix: fixed deployment issue

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,6 @@
 import server from "../dist/server/server.js";
 
-export default server.fetch;
+export default async (req: Request) => {
+  const url = new URL(req.url, `https://${req.headers.get("host")}`);
+  return server.fetch(new Request(url, req));
+};
